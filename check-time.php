092e853cfb60f804ea59a3edc2baeb315c29281e
Osvaldo Alves <oalvesxp@gmail.com>
2023-01-26 13:25:06 -0300

    Creating a script to calculator time of execution of the scripts PHP.

---
+++ check-time.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file: check_time.php
+ * @author: Osvaldo Alves
+ * @date: 26/01/2022
+ * @description: Calculating the time of exection of a script php.
+ * @use: include this file in the top of the script that you want, call the function startExec() and on in the end of script call the function endExec(). Before run the script.
+ *
+ * The function endExec() returns the result of running in seconds.
+ 	* 
+ 	* Requires the PHP >= 5 version.
+ *
+ */
+
+?>
+
