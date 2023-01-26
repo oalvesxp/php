425af421cfabc3fe4ee72c5e7f44c02bee3861dd
Osvaldo Alves <oalvesxp@gmail.com>
2023-01-26 16:54:53 -0300

    new exercise using if/else.

---
+++ aula01/if-else.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file: if-else.php
+ * @author: Osvaldo Alves
+ * @date: 26/01/2022
+ * @description: Using if/else to validate an underage person's entry to a given location based on two conditions.
+ * 1- Be over 18 years old. 
+ * 2- Be over 16 years old and accompanied by an adult.
+ * 
+ * @use: Run this script with command:> php if-else.php
+ *
+ * More informations.
+ 	*
+ 	* Requires the PHP >= 5 version.
+ *
+ */
+
+?>
