d93e0007d31b64a44ea0d0ceda7334487df3baa2
Osvaldo Alves <oalvesxp@gmail.com>
2023-01-26 16:28:16 -0300

    Study text tab.

---
+++ 01-introduction/text-tab.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @file: model.php
+ * @author: Osvaldo Alves
+ * @date: 26/01/2022
+ * @description: ...
+ * @use: Tutorial how to use.
+ *
+ * More informations.
+ 	*
+ 	* Requires the PHP >= 5 version.
+ *
+ */
+
+$age = 25;
+
+echo 'Minha idade atual é ' . $age . ' anos!';
+
+?>
