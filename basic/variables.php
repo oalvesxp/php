6adbd5a2eaa29fd0ba1ad2065338c6acac1d427d
Osvaldo Alves <oalvesxp@gmail.com>
2023-01-26 18:24:54 -0300

    Basic scripts to start larn PHP. Variables.

---
+++ basic/variables.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @file: variables.php
+ * @author: Osvaldo Alves
+ * @date: 26/01/2023
+ * @description: Using variables with string.
+ * @use: Run this script with command:> php variables.php
+ *
+ * More informations.
+ 	*
+ 	* Requires the PHP >= 5 version.
+ *
+ */
+
+$idade = 24;
+
+echo "O Osvaldo acabou de fazer $idade anos de idade! ";
