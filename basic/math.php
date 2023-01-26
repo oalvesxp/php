4a142f9b14d1e5bd26193859aa1505cbaba59cbe
Osvaldo Alves <oalvesxp@gmail.com>
2023-01-26 18:25:37 -0300

    Basic scripts to start larn PHP. Logical operators.

---
+++ basic/math.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @file: math.php
+ * @author: Osvaldo Alves
+ * @date: 26/01/2023
+ * @description: How to use arithmetic logical operators
+ * @use: Run this script with command:> php math.php
+ *
+ * More informations.
+ 	*
+ 	* Requires the PHP >= 5 version.
+ *
+ */
+
+$idade = 26;
+$idadeLater = $idade + 5;
+$dateNasc = 2023 - $idade;
+
+echo "Osvaldo nasceu no ano de $dateNasc e terá $idadeLater anos, daqui 5 anos! ";
+
+
+/**
+ * Operadores lógicos aritméticos.
+ */
+
+$soma = 2 + 2;
+$subtracao = 4 - 2;
+$multiplicacao = 3 * 5;
+$divisao = 12 / 4;
+$potencia = 2 ** 3;
+$sobraDivisao = 10 % 3;
