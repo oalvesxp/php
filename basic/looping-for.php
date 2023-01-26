1d1924314acccf1e2553d6a447d69e0c7eb07051
Osvaldo Alves <oalvesxp@gmail.com>
2023-01-26 18:26:15 -0300

    Basic scripts to start larn PHP. Lopping with method FOR.

---
+++ basic/looping-for.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @file: looping-for.php
+ * @author: Osvaldo Alves
+ * @date: 26/01/2023
+ * @description: How to do a looping with method for.
+ * @use: Run this script with command:> php looping-for.php
+ *
+ * More informations.
+ 	*
+ 	* Requires the PHP >= 5 version.
+ *
+ */
+
+for ($i = 1; $i <= 15; $i++) {
+	echo "#$i" . PHP_EOL;
+}
+
+?>
