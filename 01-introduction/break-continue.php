0e09683ff037bf0fddd3af83d7e6f416032ed3a8
Osvaldo Alves <oalvesxp@gmail.com>
2023-01-26 13:40:15 -0300

    01-introduction/break-continue.php
    changing comments from break-continue.php file

--- 01-introduction/break-continue.php
+++ 01-introduction/break-continue.php
@@ -1,10 +1,18 @@
 <?php
 
 /**
- * Created by vim
- * User: oalves
- * Date: 26/01/2022
- * time: 12:56 PM
+ * @file: break-continue.php
+ * @author: Osvaldo Alves
+ * @date: 26/01/2022
+ * @description: Study to understand how a looping behaves.
+ * @use: Run with command:> php break-continue.php.
+ *
+ * This looping makes a count in the variable $i from 1 to 15, when it reaches the value 7 it goes through an if/else condition where it will be defined tha it must 'continue'.
+ * In the second condition, a break was added when the value is equal to 13.
+ * Finally, the value of the $i variable will be printed when the looping is stopped.
+ 	* 
+ 	* Requires the PHP >= 5 version.
+ *
  */
 
 for ($i = 1; $i <= 15; $i++) {
